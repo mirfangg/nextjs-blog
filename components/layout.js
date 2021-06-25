66de1e4049e5495a491e9acc1077a7c496b6caac
Muhammad Irfan <49940969+mirfangg@users.noreply.github.com>
2021-06-25 17:50:34 +0700

    Change name

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '@irfannggg'
+const name = 'Muhammad Irfan'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
